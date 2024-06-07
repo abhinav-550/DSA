3dfa5ee826969f30b7e3964dc1d3aad2b970e628
abhinav550 <maabhinav550@gmail.com>
2024-06-07 23:40:55 +0530

    Completed BinaryTrees

---
+++ 1-ds/binaryTree/1theory/7morrisTraversal.cpp
@@ -0,0 +1,34 @@
+#include "C:\Users\Abhinav\Desktop\VS Code Projects\C C++ DSA\3.DSA in C++ Advanced\1-ds\binaryTree\1theory\0import.cpp"
+
+void morrisTraversal(Node* root){
+    Node* curr = root;
+    Node* prev;
+    while(curr != NULL){
+        if(curr -> left == NULL){
+            cout << curr->data << ' ';
+            curr = curr -> right;
+        }else{
+            prev = curr -> left;
+            while(prev->right != NULL && prev -> right != curr){
+                prev = prev -> right;
+            }
+            if(prev -> right == NULL){
+                prev -> right = curr ;
+                curr = curr -> left;
+            }else{
+                prev -> right = NULL;
+                cout << curr -> data <<' ';
+                curr = curr -> right;
+            }
+        }
+    }
+}
+
+
+int main(){
+
+Node* root = NULL;
+buildTreeFromLevelOrder(root);
+morrisTraversal(root);
+  return 0;
+}
